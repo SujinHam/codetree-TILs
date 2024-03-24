068fff7b997478aacaf17189a51d5b777326fa34
SujinHam <130124538+SujinHam@users.noreply.github.com>
2024-03-25 01:48:17 +0900

    Title: 정사각형의 넓이, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240325/정사각형의 넓이/area-of-a-rectangle.java
+++ 240325/정사각형의 넓이/area-of-a-rectangle.java
@@ -3,10 +3,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        if (n >= 5) {
-            System.out.println(n*n);
-        }
-        else if (n < 5) {
+        System.out.println(n*n);
+        if (n < 5) {
             System.out.println("tiny");
         }
     }
